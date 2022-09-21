f1a5a556868e23d150ef9cc0158e53742960ba08
azzar <azzar.mr.zs@gmail.com>
2022-09-21 18:46:48 +0700

    ...

--- smart_relays/smart_relays.ino
+++ smart_relays/smart_relays.ino
@@ -72,23 +72,3 @@ void loop() {
     }
   }
 }
-
-void MQTT_connect() {
-  int8_t ret;
-  if (mqtt.connected()) {
-    return;
-  }
-  Serial.print("Connecting to MQTT... ");
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) {
-    Serial.println(mqtt.connectErrorString(ret));
-    Serial.println("Retrying MQTT connection in 5 seconds...");
-    mqtt.disconnect();
-    delay(5000);
-    retries--;
-    if (retries == 0) {
-      while (1);
-    }
-  }
-  Serial.println("MQTT Connected!");
-}
